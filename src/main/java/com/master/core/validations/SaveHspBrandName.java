1ffa3f896637a0f2884ce0d97dadd216c94b5ffd
sandhya <tmt.5@qubehealth.com>
2024-05-25 14:52:15 +0530

    not null validation

--- src/main/java/com/master/core/validations/SaveHspBrandName.java
+++ src/main/java/com/master/core/validations/SaveHspBrandName.java
@@ -15,12 +15,12 @@ public class SaveHspBrandName {
     @JsonProperty("hsp_id")
     private Integer hspId;
 
-    @NotEmpty(message = "transaction_id cannot be empty")
+    @NotNull(message = "transaction_id cannot be empty")
     @Pattern(regexp = ".*\\D.*", message = "transaction_id must be a String")
     @JsonProperty("transaction_id")
     private String transactionId;
 
-    @NotEmpty(message = "hsp_brand_name cannot be empty")
+    @NotNull(message = "hsp_brand_name cannot be empty")
     @Pattern(regexp = ".*\\D.*", message = "hsp_brand_name must be a String")
     @JsonProperty("hsp_brand_name")
     private String hspBrandName;
