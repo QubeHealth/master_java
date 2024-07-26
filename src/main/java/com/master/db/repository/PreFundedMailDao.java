771f708642ef3287172ed7f8c9034ce90f892a6f
Dhiraj Jadhav <tmt.9@qubehealth.com>
2024-07-26 11:50:09 +0530

    CHANGES

--- src/main/java/com/master/db/repository/PreFundedMailDao.java
+++ src/main/java/com/master/db/repository/PreFundedMailDao.java
@@ -6,6 +6,6 @@ import com.master.core.constants.Queries;
 
 public interface PreFundedMailDao {
     
-    @SqlQuery(Queries.GET_METADATA)
+    @SqlQuery(Queries.GET_EMAILER_ITEMS_METADATA)
     String getMetaData(@Bind("claim_no") String claimNo);
 }
